b73e7791dd0d37e8e45d85869c82acf159b0c7ac
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-04-05 12:54:07 +0500

    ...

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import inquirer from "inquirer";
 ;
 //array
-let todolist = [];
+const todolist = [];
 //function
 async function mainMenu() {
     const { action } = await inquirer.prompt({
@@ -20,27 +20,42 @@ async function mainMenu() {
         case 'Mark as completed':
             await markCompleted();
             break;
-        case 'Delete Task':
-            break;
+        //  case 'Delete Task':
+        //     await deleteTask()
+        //      break;
         case 'Exit':
             console.log('Goodbye');
             return;
     }
+    mainMenu();
 }
-mainMenu();
 let addTask = async () => {
     let { task } = await inquirer.prompt({
         type: 'input',
         name: 'task',
         message: 'Enter your Task',
     });
-    todolist.push({ task, completed: false });
+    todolist.push({ task, completed: true });
     console.log("task added successfuly");
 };
 let viewList = () => {
     console.log("**** To Do List ****");
-    todolist.forEach(item, index);
+    todolist.forEach((item, index) => {
+        console.log(`${index + 1}.[${item.completed ? 'x' : ''}] ${item.task}`);
+    });
+    console.log("*****************************");
 };
-{
-    console.log(`${index + y1}.[${item.completed}]`);
-}
+let markCompleted = async () => {
+    let { myIndex } = await inquirer.prompt([{
+            type: "number",
+            name: "myIndex",
+            message: "Which task you wont to mark as completed ?",
+        }]);
+    if (myIndex < 1 || todolist.length) {
+        console.log("Invalid task number. 'please try again'");
+        return;
+    }
+    todolist[myIndex - 1].completed = true;
+    console.log("task mark as completed ");
+};
+mainMenu();
